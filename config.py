11fe874132a5e854576d110c852fce7398f908e8
Cole Murray <colemurray.cs@gmail.com>
2017-01-29 00:39:15 -0800

    Add App Engine FrontEnd

---
+++ config.py
@@ -0,0 +1,5 @@
+PROJECT_ID = 'photo-recog'
+CLOUD_STORAGE_BUCKET = 'photo-recog'
+MAX_CONTENT_LENGTH = 8 * 1024 * 1024
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif'])
+PREDICTION_SERVICE_URL = 'http://104.198.43.227:80'
